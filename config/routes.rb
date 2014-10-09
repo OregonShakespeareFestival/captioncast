c89878bf4f42f9c58438d3bce71f975cbcee9c6b
Joel Ferrier <jferrier45@gmail.com>
2014-10-09 11:22:33 -0700

    run rails_admin generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   root 'cast#index'
 
   get 'cast/index'
   get 'cast/about'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
