d168c3c1bcd50a1afef339aae6973a6a3e5f3121
Joel Ferrier <jferrier45@gmail.com>
2014-10-09 11:09:37 -0700

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'cast/index'
+
   root 'cast#index'
+
+  get 'cast/index'
+  get 'cast/about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
