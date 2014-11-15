a2a2dc36dec7e9cb7ad1db0f3f91d7d6760aa8ca
Andrew Krug <andrewkr@osfashland.org>
2014-11-15 09:34:04 -0800

    Now concats character on front of line.  Still need to strip stage directions.

--- nokotest.sh
+++ nokotest.sh
@@ -27,19 +27,57 @@ end
 
 #/FinalDraft/Content/Paragraph//Text
 
+#XPath any sub-item of node type paragraph that has attribute character containing text.
+characters = doc.xpath("/FinalDraft/Content/Paragraph[@Type='Character']//Text")
+
+#Create a character hash
+hcharacters = Array.new
+
+#Print the character list
+characters.each do |character|
+  hcharacters.push(character.to_str)
+end
+
+#Make sure only unique characters are in the array.
+hcharacters = hcharacters.uniq
+
+#If we're in debug print out all the characters
+if (debug == "1")
+  puts hcharacters
+else
+end
+
+#XPath any sub-item of node type paragraph containing text.
 items = doc.xpath("/FinalDraft/Content/Paragraph//Text")
 
+#Counter int for the lines
 linenum = 0
 
+#Create a scope var to hold characters temporarily.
+
+linecharacter = ""
+
+#For each item let's bulding a string object with the line number and line.
 items.each do |item|
+  #Cast our other itmes as strings.
   l = linenum.to_s
   s = item.to_str
 
+  #Search the array to see if the line is actually a Character.
+  check = hcharacters.any? { |w| s =~ /^#{w}$/ }
+
+  if check == true
+    linecharacter = s
+    next
+  else
+
+  end
+
+  #If it's a new line let's go to the next item.
   if s == ("")
     next
   else
-    puts item.type
     linenum = linenum + 1
-    puts l + " " + s
+    puts l + " " + linecharacter + ": " + s
   end
 end
