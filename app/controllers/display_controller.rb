be4ce784961c70b48b2a9bbbef666c70a90cfb63
Andrew Krug <andrewkr@osfashland.org>
2014-11-16 11:46:11 -0800

    fully commented js and erb

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -1,23 +1,12 @@
 class DisplayController < ApplicationController
-
   def index
-
+    #This gets returned to the view for the index.
     @atext = Text.where(visibility: true)
-
-    position = 8
-    t = fetchline(position)
-
-    if t.visibility.to_s == "true"
-      @currentcaption = t.content_text
-    else
-      @currentcaption = "nontext"
-    end
-
   end
 
+  #Function to pull back a particular noko-obj using the ID in the database to return the element.
   def fetchline(num)
     s = Text.find(num)
     return s
   end
-
 end
