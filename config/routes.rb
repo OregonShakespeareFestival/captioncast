6c82e1a225a2340899618ca212a528d3e1aafa7b
Andrew Krug <andrewkr@osfashland.org>
2014-11-16 10:28:22 -0800

    javascript on the display index controller sort of works to display hidden spans

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Rails.application.routes.draw do
 
   get 'upload/new'
 
+
+  get 'display/index'
+  get 'display/next'
+  get 'display/previous'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  
+
   root 'cast#index'
 
   get 'cast/index'
   get 'cast/about'
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
