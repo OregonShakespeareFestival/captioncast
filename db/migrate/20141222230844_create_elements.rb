aac999862062890aa33b758b3c89ee4187fe0884
Andrew Krug <andrewkr@osfashland.org>
2014-12-22 16:13:02 -0800

    changed type to e_type in elements mig

--- db/migrate/20141222230844_create_elements.rb
+++ db/migrate/20141222230844_create_elements.rb
@@ -1,7 +1,7 @@
 class CreateElements < ActiveRecord::Migration
   def change
     create_table :elements do |t|
-	t.string :type
+	t.string :e_type
 	t.integer :content_text
 	t.string :color
       t.timestamps
