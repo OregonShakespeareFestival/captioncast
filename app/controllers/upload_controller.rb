79a0c2784daa123010a6dc455623014bfdce2ccf
Andrew Krug <andrewkr@osfashland.org>
2014-12-23 13:11:32 -0800

    moving xpath to data_file model

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,10 +1,14 @@
 class UploadController < ApplicationController
+
   def index
     render :file => 'app/views/upload/uploadfile.html.erb'
   end
 
   def uploadFile
     post = DataFile.save(params[:upload])
+    post = DataFile.parse_fd(params[:upload],"Equivocation")
     render :text => "File has been uploaded successfully"
   end
+
+
 end
