f4272f0b081dc542bd6331a03ea0be3e0807f004
randolph-osf <vagrant@localhost.localdomain>
2014-12-23 10:37:21 -0800

    added element_name attribute to element

--- db/migrate/20141222230844_create_elements.rb
+++ db/migrate/20141222230844_create_elements.rb
@@ -1,6 +1,7 @@
 class CreateElements < ActiveRecord::Migration
   def change
     create_table :elements do |t|
+      t.string :element_name
       t.string :element_type
       t.string :color
       t.timestamps
