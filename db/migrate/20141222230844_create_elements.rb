23e23ea9df08267f5444956b40a8c8903adecf08
Joel Ferrier <jferrier45@gmail.com>
2014-12-22 23:00:30 -0800

    complete relations

--- db/migrate/20141222230844_create_elements.rb
+++ db/migrate/20141222230844_create_elements.rb
@@ -1,9 +1,10 @@
 class CreateElements < ActiveRecord::Migration
   def change
     create_table :elements do |t|
-	t.string :element_type
-	t.string :color
+      t.string :element_type
+      t.string :color
       t.timestamps
     end
+    add_reference :elements, :work, index: true
   end
 end
