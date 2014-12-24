d5161ae268d17ade136b22d60c291e528876080a
Joel Ferrier <jferrier45@gmail.com>
2014-12-23 17:16:11 -0800

    select work from upload page

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,14 @@ lizzy  = Venue.find_or_create_by!(name: 'Allen Elizabethan Theatre')
 
 equivocation = Work.find_or_create_by!(name: 'Equivocation') { |work| work.venue = bowmer }
   
-dialogue = Element.find_or_create_by!(element_type: 'Dialogue') { |dialogue| dialogue.color = '#333333'; dialogue.work = equivocation }
+dialogue = Element.find_or_create_by!(element_type: 'Dialogue') { |element| element.color = '#333333'; element.work = equivocation }
+non_dialogue = Element.find_or_create_by!(element_type: 'Non-Dialogue') { |element| element.color = '#333333'; element.work = equivocation }
+act_break = Element.find_or_create_by!(element_type: 'Act Break') { |element| element.color = '#333333'; element.work = equivocation }
+scene_heading = Element.find_or_create_by!(element_type: 'Scene Heading') { |element| element.color = '#333333'; element.work = equivocation }
+character = Element.find_or_create_by!(element_type: 'Character') { |element| element.color = '#333333'; element.work = equivocation }
+action = Element.find_or_create_by!(element_type: 'Action') { |element| element.color = '#333333'; element.work = equivocation }
+parenthetical = Element.find_or_create_by!(element_type: 'Parenthetical') { |element| element.color = '#333333'; element.work = equivocation }
+operator_note = Element.find_or_create_by!(element_type: 'Operator Note') { |element| element.color = '#333333'; element.work = equivocation }
 
 text   = Text.find_or_create_by!( content_text: 'Why me?') { |text| text.visibility = true; text.sequence = 1; text.work = equivocation; text.element = dialogue }
 text   = Text.find_or_create_by!( content_text: 'It wasn’t that others weren’t considered.') { |text| text.visibility = true; text.sequence = 2; text.work = equivocation; text.element = dialogue }
