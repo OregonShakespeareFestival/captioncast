0a6f65bba7123813386e81922e01b36110ba9860
randolph-osf <sneakyjones@JZSDK02.orshakes.org>
2014-12-30 15:46:39 -0800

    added basic preview tab to operator view

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -10,6 +10,24 @@ class DataFile < ActiveRecord::Base
     File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
   end
 
+
+  #add element to the database
+  def self.add_element(name, type, color, work)
+      #Text.create(sequence: linenum, content_text: linecharacter + ": " + s, visibility: true, element: Element.find_by(element_type: 'Dialogue'), work: Work.find_by_name('Equivocation'))
+      e = Element.find_or_create_by!(element_name: name, element_type: type, color: color, work: Work.find_by_name(work))
+      e.save
+  end 
+  
+
+
+  # get the characters in the play
+
+  # get the name/title of the play
+
+  # get the color of each character 
+
+
+
   def self.parse_fd(upload,work)
 
     name =  upload['datafile'].original_filename
@@ -26,70 +44,27 @@ class DataFile < ActiveRecord::Base
     #Build an xpath for what we want to search for
     #/FinalDraft/Content/Paragraph//Text
     #Nondialogue Elements
-    nondialouge = doc.xpath("/FinalDraft/Content/*[not(@Type='Dialogue' or @Type='Character')]//Text")
-      hnondialougue = Array.new
+    nondialouge = doc.xpath("/FinalDraft/Content/*[not(@Type='Dialogue')]")
+      #hnondialougue = Array.new
 
+
+    #gets any non dialougge element
     nondialouge.each do |nondialouge|
-      hnondialougue.push(nondialouge.to_str)
+      # assign and check each element
+      type = nondialouge.attributes["Type"].value
+      #add element to the database if not already there
+      self.add_element("", type.to_str.upcase, "#666666", "Equivocation")
     end
+
 
     #XPath any sub-item of node type paragraph that has attribute character containing text.
     characters = doc.xpath("/FinalDraft/Content/Paragraph[@Type='Character']//Text")
 
-    #Create a character hash
-    hcharacters = Array.new
-
-    #Push each elem to array
+    #Adds each character to the database if it's new to the database
     characters.each do |character|
-      hcharacters.push(character.to_str)
-    end
-
-    #Make sure only unique characters are in the array.
-    hcharacters = hcharacters.uniq
-
-    #XPath any sub-item of node type paragraph containing text.
-    items = doc.xpath("/FinalDraft/Content/Paragraph//Text")
-
-    #Counter int for the lines
-    linenum = 0
-
-    #Create a scope var to hold characters temporarily.
-
-    linecharacter = ""
-
-    #For each item let's bulding a string object with the line number and line.
-    items.each do |item|
-      #Cast our other itmes as strings.
-      l = linenum.to_s
-      s = item.to_str
-
-      #Search the array to see if the line is actually a Character.
-      charcheck = hcharacters.any? { |w| s =~ /^#{w}$/ }
-
-      if charcheck == true
-        linecharacter = s
-        next
-      else
-        #If it's not a character better check to see if it's non-dialogue.
-        nondial = hnondialougue.include?(s)
-        if nondial == true
-          txt = Text.create(sequence: linenum, content_text: linecharacter + ": " + s, visibility: false, element: Element.find_by(element_type: 'Dialogue'), work: Work.find_by_name('Equivocation'))
-          txt.save
-          linenum = linenum + 1
-          next
-        end
-      end
-
-      #If it's a new line let's go to the next item.
-      if s == ("")
-        next
-      else
-        linenum = linenum + 1
-        text = l + " " + linecharacter + ": " + s
-        txt = Text.create(sequence: linenum, content_text: linecharacter + ": " + s, visibility: true, element: Element.find_by(element_type: 'Dialogue'), work: Work.find_by_name('Equivocation'))
-        txt.save
-        #In final for all dialogue we should set visibility to true.
-      end
+      name = character.text.to_str.upcase
+      self.add_element( name, "Character", "#111111", "Equivocation")
+    
     end
   end
 end
