c6c8a3ba9e4670eea809817fedad2d5c4d3d62a1
randolph-osf <sneakyjones@JZSDK02.orshakes.org>
2014-12-30 15:46:39 -0800

    added basic preview tab to operator view

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,25 +20,3 @@ character = Element.find_or_create_by!(element_type: 'Character') { |element| el
 action = Element.find_or_create_by!(element_type: 'Action') { |element| element.color = '#333333'; element.work = equivocation }
 parenthetical = Element.find_or_create_by!(element_type: 'Parenthetical') { |element| element.color = '#333333'; element.work = equivocation }
 operator_note = Element.find_or_create_by!(element_type: 'Operator Note') { |element| element.color = '#333333'; element.work = equivocation }
-
-text   = Text.find_or_create_by!( content_text: 'Why me?') { |text| text.visibility = true; text.sequence = 1; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'It wasn’t that others weren’t considered.') { |text| text.visibility = true; text.sequence = 2; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'I can’t make the decision') { |text| text.visibility = true; text.sequence = 3; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'Why not?') { |text| text.visibility = true; text.sequence = 4; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'We’re a cooperative venture.') { |text| text.visibility = true; text.sequence = 5; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'Who’s in charge?') { |text| text.visibility = true; text.sequence = 6; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'We all share equally in the income and responsibilities of the theater...We’re a cooperative venture.') { |text| text.visibility = true; text.sequence = 7; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'Who’s in charge?') { |text| text.visibility = true; text.sequence = 7; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'Richard.') { |text| text.visibility = true; text.sequence = 8; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'This will take care of Richard...One week.') { |text| text.visibility = true; text.sequence = 9; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'One?') { |text| text.visibility = true; text.sequence = 10; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'He said/she said. Enter/exit. Drums/trumpets. How long can it take?') { |text| text.visibility = true; text.sequence = 11; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: ' You have one week to “dialogue” this.') { |text| text.visibility = true; text.sequence = 12; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'We’re already working on a new play.') { |text| text.visibility = true; text.sequence = 13; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'About?') { |text| text.visibility = true; text.sequence = 14; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'A king.') { |text| text.visibility = true; text.sequence = 15; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'How does he die?') { |text| text.visibility = true; text.sequence = 16; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'What makes you think he dies?') { |text| text.visibility = true; text.sequence = 17; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'You’ve killed more kings than any man alive. Your brain is a graveyard for royalty.') { |text| text.visibility = true; text.sequence = 18; text.work = equivocation; text.element = dialogue }
-text   = Text.find_or_create_by!( content_text: 'This one dies of a broken heart.') { |text| text.visibility = true; text.sequence = 19; text.work = equivocation; text.element = dialogue }
-
