1003904c7816698ae1647686cee03cf89778181d
Andrew Krug <andrewkr@osfashland.org>
2015-01-19 12:47:45 -0800

    now has language support in works table

--- db/migrate/20141222230826_create_works.rb
+++ db/migrate/20141222230826_create_works.rb
@@ -3,6 +3,7 @@ class CreateWorks < ActiveRecord::Migration
     create_table :works do |t|
       t.string :name
       t.timestamps
+      t.string :language
     end
     add_reference :works, :venue, index: true
   end
