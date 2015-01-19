0680b6f081429097ad9f53a1f545c3804be457f3
Joel Ferrier <jferrier45@gmail.com>
2015-01-19 15:23:12 -0800

    remove unused routes/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'upload/new'
   get 'upload/index'
   post 'upload/index'
   post 'upload/uploadFile'
   get 'display/index'
-  get 'display/next'
-  get 'display/previous'
-  get 'display/current'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
