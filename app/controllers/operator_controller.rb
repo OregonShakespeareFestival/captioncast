332a820cc7a3ed39dfdcebdc8069546dbfb61049
Joel Ferrier <jferrier45@gmail.com>
2015-01-22 10:13:28 -0800

    create operator when work is selected

--- app/controllers/operator_controller.rb
+++ app/controllers/operator_controller.rb
@@ -1,7 +1,7 @@
 class OperatorController < ApplicationController
 	def index
 		#convert all lines to json and pass along in variable
-		#disabled at the moment so fixture data can be used		
+		#disabled at the moment so fixture data can be used
 
 		@jtext = Text.all.where(work: params[:work]).to_json(:include => :element);
 	end
@@ -13,10 +13,20 @@ class OperatorController < ApplicationController
 		#render :nothing => true
 	end
 	def select
-		#todo create operator record
-		#call index, passing params[work] to it
-		if request.post?			
-			redirect_to :controller => 'operator', :action => 'index', :work => params[:work]
+
+		if request.get?
+			#TODO: remove old operator records when we load this page
+		end
+
+		if request.post?
+			# Create Operator record
+			operator_name = params[:operator]
+			Operator.create!(name: operator_name[:name],
+				view_attributes: params[:view], work_id: params[:work])
+
+			# redirect to index
+			redirect_to :controller => 'operator', :action => 'index',
+				:work => params[:work], :view_mode => params[:view]
 		end
 	end
 end
