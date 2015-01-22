d0581e33536fffd392a1c977af0c53562d8a0b21
Joel Ferrier <jferrier45@gmail.com>
2015-01-22 14:43:56 -0800

    trim old sessions, allow reopening a session

--- app/controllers/operator_controller.rb
+++ app/controllers/operator_controller.rb
@@ -8,7 +8,6 @@ class OperatorController < ApplicationController
 		# slurp up the required text
 		@jtext = Text.all.where(work: params[:work]).to_json(:include => :element)
 		# add the default position of 0 for an operator
-		Rails.application.config.operator_positions.merge!({params[:operator] => "0"})
 	end
 	def pushTextSeq
 		Rails.application.config.operator_positions.merge!({params[:operator] => params[:seq]})
@@ -17,22 +16,45 @@ class OperatorController < ApplicationController
 		#when we're in production and don't need a reply
 		#render :nothing => true
 	end
-	
-	def select
 
+	def select
+		@operators = Operator.all
 		if request.get?
-			#TODO: remove old operator records when we load this page
+			now = DateTime.now
+			@operators.each do |operator|
+        # if the operator record is more than a day old remove it
+				if operator.updated_at < now-1
+					operator.destroy!
+				end
+			end
+			#pull back new list of operators
+			@operators = Operator.all
 		end
 
 		if request.post?
-			# Create Operator record
-			operator_name = params[:operator]
-			operator = Operator.create!(name: operator_name[:name],
-				view_attributes: params[:view], work_id: params[:work])
+			if !params[:selected_operator].nil?
+				# load exisiting operator session
+				operator = Operator.find_by(id: params[:selected_operator])
+				operator_name = operator.name
+				view_mode = operator.view_attributes
+				work = operator.work_id
+				Rails.application.config.operator_positions.merge!({params[:operator] => "0"})
+			else
+				# Create Operator record
+				operator_name = params[:operator]
+				view_mode = params[:view]
+				work = params[:work]
+				#TODO: check to see if we will be creating a duplicate
 
+				operator = Operator.create!(name: operator_name[:name],
+					view_attributes: params[:view], work_id: params[:work])
+			end
+
+			# set position to 0
+			Rails.application.config.operator_positions.merge!({operator.id => "0"})
 			# redirect to index
 			redirect_to :controller => 'operator', :action => 'index',
-				:work => params[:work], :view_mode => params[:view],
+				:work => work, :view_mode => view_mode,
 				:operator => operator.id
 		end
 	end
