4baf2265da52f2af571619c23cac974a65cdd09c
randolph-osf <sneakyjones@JZSDK02.orshakes.org>
2015-01-23 09:34:04 -0800

    up and down arrows automatically commit lines

--- app/assets/javascripts/operator.js
+++ app/assets/javascripts/operator.js
@@ -13,10 +13,40 @@ var scrolling = false;
 //var reserveLine = 1;
 //var lineMapping;
 
-$(document).ready(function(){
 
+$(document).ready(function(){
 	//if we're in the operator view
 	if($('#main-operator').length>0){
+		//this function will universally commit the targeted line
+		function commit(){
+				//post the current sequence identifier through AJAX
+				if($('.target-operator').attr('data-visibility')=="true"){
+					$.ajax('/operator/pushTextSeq', {
+						type:'POST',
+						data: {
+							seq:$('.target-operator').attr('data-sequence'),
+	            operator: operator
+						},
+						success:(function(d){
+							//display logic if successful
+							$('.current-operator').removeClass('current-operator');
+							$('.target-operator').addClass('current-operator');
+							if(blackout){
+								$('#blackout-icon-operator').addClass('blackout-off-operator');
+								blackout=false;
+							}
+							current=$('.target-operator').attr('data-sequence');
+						}),
+						error:(function(){
+							//put in better error handling here
+							alert('Commit failed! Please check your connection.');
+						}),
+					});
+
+				}
+
+		}
+
 		$('#main-operator').height($(window).innerHeight()+'px');
 
 		//console.log('this is the operator view');
@@ -28,17 +58,6 @@ $(document).ready(function(){
 			return q.sequence;
 		});
 
-		//waiting to get data vended in for characters
-
-		//probably won't need this but hanging onto it for now
-		//split each line into content and character. Assume 0 to first colon in line is character name
-		/*
-		_.each(lines, function(q, i){
-			lines[i].character = q.content_text.substring(0, q.content_text.indexOf(':'));
-			lines[i].line = q.content_text.substring(0, q.content_text.indexOf(':'));
-
-		});
-		*/
 		//templating per line
 		_.each(lines, function(q, i){
 			//this is a temporary scrub in of fixture characters
@@ -51,18 +70,10 @@ $(document).ready(function(){
 			}else{
 				q.character=' ';
 			}
-
-
-			//console.log(q.content_text);
-			//if the line is visible then show it?
-			//maybe we want it just a different color for operator view
-			//evens and odds?
-			if(i%2==0){
-				q.even=true;
-
-			}else{
-				q.even=false;
-			}
+			$('#line-holder-sub-operator').append(
+				tLine(q)
+			);
+			/*
 			if(q.visibility){
 				$('#line-holder-sub-operator').append(
 					tLine(q)
@@ -71,7 +82,7 @@ $(document).ready(function(){
 				//need handling for non-visible items
 				//show them with different styling
 				//
-			}
+			}*/
 			$('.line-operator').first().addClass('target-operator');
 			$('.line-operator').each(function(){
 				if($(this).attr('data-visibility')=="false"){
@@ -80,31 +91,7 @@ $(document).ready(function(){
 			});
 		});
 		//this happens when you click the commit button
-		$('#commit-button-operator').click(function(){
-			//post the sequence of the selected line via ajax
-			console.log($('.target-operator').attr('data-visibility'));
-			if($('.target-operator').attr('data-visibility')=="true"){
-				$.ajax('/operator/pushTextSeq', {
-					type:'POST',
-					data: {
-						seq:$('.target-operator').attr('data-sequence'),
-            operator: operator
-					},
-					success:(function(d){
-						//console.log('line pushed ' + d);
-					}),
-				});
-
-				$('.current-operator').removeClass('current-operator');
-
-				$('.target-operator').addClass('current-operator');
-				if(blackout){
-					$('#blackout-icon-operator').addClass('blackout-off-operator');
-					blackout=false;
-				}
-				current=$('.target-operator').attr('data-sequence');
-			}
-		});
+		$('#commit-button-operator').click(commit);
 		//scrolling target feature
 		$('#line-holder-operator').scroll(function(){
 			var updateInt = 100;
@@ -279,6 +266,10 @@ $(document).ready(function(){
 						{scrollTop:
 							$('#line-holder-operator').scrollTop() - diff
 						}, scrollSpd, function(){
+							if(autoCommit){
+								commit();
+							}
+
 							scrolling=false;
 						});
 				}
@@ -304,6 +295,10 @@ $(document).ready(function(){
 						{scrollTop:
 							$('#line-holder-operator').scrollTop() - diff
 						}, scrollSpd, function(){
+							if(autoCommit){
+								commit();
+							}
+
 							scrolling=false
 						});
 				}
