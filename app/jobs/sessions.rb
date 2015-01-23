8987793c91c645471ccc8bb5a75bad102a52eb9f
Joel Ferrier <jferrier45@gmail.com>
2015-01-22 17:58:55 -0800

    write job that culls operator sessions older than 6 hrs

---
+++ app/jobs/sessions.rb
@@ -0,0 +1,15 @@
+class Sessions
+  @queue = :session_managment
+
+  def self.perform
+    expiry_time = (DateTime.now.to_time - 6.hours).to_datetime
+    operators = Operator.all
+    operators.each do |operator|
+      if operator.updated_at < expiry_time
+        operator.destroy!
+        Rails.application.config.operator_positions.delete(operator.id)
+      end
+    end
+  end
+
+end
