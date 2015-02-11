954b7724cb0fa3d1181a4db4625b8e907411b15a
toeknee919 <toeknee919@gmail.com>
2015-02-11 10:40:56 -0800

    now able to edit lines

---
+++ app/controllers/texts_controller.rb
@@ -0,0 +1,27 @@
+class TextsController < ApplicationController
+       
+
+       #********************************************************************
+       #updates the fields in the Text table when the editor changes the lines in a script
+       #******************************************************************
+       def update
+          @text2 = Text.find(params[:id])
+          if @text2.update_attributes(message_params)
+	          # Handle a successful update.
+	          flash[:notice] = "Edit successfully made"
+	          redirect_to:back 
+          else
+          	  flash[:notice] = "NOTICE: ERROR DURING UPDATE"
+	          redirect_to:back
+          end 
+       end
+
+
+       private
+       #********************************************************************
+       #used for bringing in params sent to the create function
+       #******************************************************************
+	   def message_params
+	      params.require(:text).permit(:content_text)
+	   end
+end
--- app/models/work.rb
+++ app/models/work.rb
@@ -4,6 +4,7 @@ class Work < ActiveRecord::Base
   validates_presence_of :venue_id
   validates_presence_of :language
 
+  #places the name/language in the dropdown for selecting the script to edit
   def name
     "#{work_name}. #{language}"
   end
