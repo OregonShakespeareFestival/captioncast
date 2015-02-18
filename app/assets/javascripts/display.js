ceb145fcb1525522cb9ce00354107ef7ebd71623
Randolph Jones <randolphjones1029@gmail.com>
2015-02-17 23:46:23 -0800

    optimized display view some. Filters out redundant character refs now

--- app/assets/javascripts/display.js
+++ app/assets/javascripts/display.js
@@ -2,25 +2,41 @@
 var refresh = 500;
 var dispFadeSpd = 400;
 var displayScrollSpd = 1200;
+var $linesDisp;
 $(document).ready(function(){
 	if($('#body-index-display').length>0){
 		var current=0;
 
+		//make sure the lines are sorted by sequence instead of index when read in
+		lines = _.sortBy(lines,function(q){
+			return q.sequence;
+		});
+		function buildLinesDisp($container){
+			var il = 0;
+			var ll = lines.length; 
+			var lc, cc;
+			while(il<ll){
+				if(lines[il]['visibility']){
+					var cl = lines[il];
+					cc = cl['element']['element_name'];
+					if(cc.length>0&&cc!=lc){
+						cl['character'] = cl['element']['element_name'] + ':';
+						lc = cc;
+					}else{
+						cl['character']=' ';
+					}
+					$container.append(tLine(cl));
+				}
+				il++;
+			}
+		}
+
 		if($('#multi-flag-display').length<=0){
-
-		//if($('#main-display').length>0){
-			//console.log('this is the display view');
-			//template views
+			$lineCont = $('#line-holder-display');
+			// template views
 			var tLine = _.template($('#line-template-display').html());
-
-			//make sure the lines are sorted by sequence instead of index when read in
-			lines = _.sortBy(lines,function(q){
-				return q.sequence;
-			});
-
-
 			//seed sequence #0 with a blank line
-			$('#line-holder-display').append(
+			$lineCont.append(
 				tLine({
 					"character":'',
 				    "id": 0,
@@ -34,29 +50,12 @@ $(document).ready(function(){
 					})
 				);
 
-			//waiting to get data vended in for characters
-
-			//templating per line
-			_.each(lines, function(q, i){
-				//this is a temporary scrub in of fixture characters
-				//q.character = 'Character';
-				//console.log(q.character);
-				//append character name with color
-				if(q.element.element_name.length>0){
-					//console.log('I seeee you');
-					q.character = q.element.element_name + ':';
-				}else{
-					q.character=' ';
-				}
-				if(q.visibility){
-					$('#line-holder-display').append(
-						tLine(q)
-					);
-				}
-			});
+			//build the line dom
+			buildLinesDisp($lineCont);
+			$linesDisp = $('.line-display');
 			//when the first line fade in it sets off the preiodic ajax scrape
 			//for now we're just grabbing a random number but soon it will be the number supplied by the operator
-			$('.line-display').first().fadeIn(dispFadeSpd, function(){
+			$linesDisp.first().fadeIn(dispFadeSpd, function(){
 				$(this).addClass('shown-display');
 				//set first interval
 				function heartbeat(){
@@ -74,7 +73,7 @@ $(document).ready(function(){
 									$('.shown-display').fadeOut(dispFadeSpd, function(){
 										$(this).removeClass('shown-display');
 										//console.log('class-removed');
-										$(_.find($('.line-display'), function(q){
+										$(_.find($linesDisp, function(q){
 											return parseInt($(q).attr('data-sequence'))==j;
 											})).fadeIn(dispFadeSpd, function(){
 												$(this).addClass('shown-display');
@@ -105,21 +104,16 @@ $(document).ready(function(){
 		//}
 		}else{
 			$('#shade-multi').css('display', 'none');
-
 			//this is specific JS for the multi-line view
 			console.log('welcome to multi-line mode');
-			//console.log('this is the display view');
-			//template views
+			// //template views
 			var tLine = _.template($('#line-template-display-multi').html());
 
-			//make sure the lines are sorted by sequence instead of index when read in
-			lines = _.sortBy(lines,function(q){
-				return q.sequence;
-			});
+			$lineCont = $('#line-holder-display-multi');
 
 
 			//seed sequence #0 with a blank line
-			$('#line-holder-display-multi').append(
+			$lineCont.append(
 				tLine({
 					"character":'',
 				    "id": 0,
@@ -133,33 +127,10 @@ $(document).ready(function(){
 					})
 				);
 
-			//waiting to get data vended in for characters
-			//$('#line-holder-display-multi').append('<div id="buffer-top-display-multi"></div>');
+			//build the line dom
+			buildLinesDisp($lineCont);
+			$linesDisp = $('.line-display-multi');
 
-			//templating per line
-			_.each(lines, function(q, i){
-				//this is a temporary scrub in of fixture characters
-				//q.character = 'Character';
-				//console.log(q.character);
-				//append character name with color
-				if(q.element.element_name.length>0){
-					//console.log('I seeee you');
-					q.character = q.element.element_name + ':';
-				}else{
-					q.character=' ';
-				}
-				if(q.visibility){
-					$('#line-holder-display-multi').append(
-						tLine(q)
-					);
-				}
-			});
-			//$('#line-holder-display-multi').append('<div id="buffer-bottom-display-multi"></div>');
-
-			//$('.line-display-multi').addClass('shown-display');
-			//$('.line-display-multi').addClass('blur-multi');
-			//when the first line fade in it sets off the preiodic ajax scrape
-			//for now we're just grabbing a random number but soon it will be the number supplied by the operator
 
 			//set first interval
 			function heartbeat(){
@@ -177,21 +148,10 @@ $(document).ready(function(){
 
 								if(current!==0){
 									$('#shade-multi').fadeOut(dispFadeSpd);
-									var newElem = _.find($('.line-display-multi'), function(q){
+									var newElem = _.find($linesDisp, function(q){
 											return parseInt($(q).attr('data-sequence'))==j;
 									});
-								/*
-									//animate the scroll
-									var diff = ($('#line-display-0').position().top - $(newElem).position().top)*1.0;
-									$('#line-holder-display-multi').animate(
-										{scrollTop:
-											$('#line-holder-display-multi').scrollTop() - diff
-									}, displayScrollSpd);
-								*/
-									//$('#body-index-display').animate({scrollTop:$(newElem).position().top-$(window).height()/2-$(newElem).height()/2});
-									//$('#body-index-display').animate({scrollTop:$(newElem).position().top-$(newElem).height()/2});
 									$('#body-index-display').animate({scrollTop:$(newElem).position().top-$(window).height()/2+$(newElem).height()+bottomPad}, displayScrollSpd);
-									//$('.shown-display').fadeOut(dispFadeSpd, function(){
 									//remove the focus class
 									$('.focus-multi').removeClass('focus-multi');
 									//console.log('class-removed');
