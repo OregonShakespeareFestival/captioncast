d849d9fd8a1db2e3b9a9e9229e3e5b4e7317c5f1
Rob Head <rob@codingzeal.com>
2015-02-20 10:22:36 -0800

    Remove tabs, re-indent.

--- app/controllers/operator_controller.rb
+++ app/controllers/operator_controller.rb
@@ -1,16 +1,16 @@
 class OperatorController < ApplicationController
-	def index
-		#convert all lines to json and pass along in variable
-		#disabled at the moment so fixture data can be used
+  def index
+    #convert all lines to json and pass along in variable
+    #disabled at the moment so fixture data can be used
 
-		#set the operator variable
-		@operator = params[:operator]
-		@view_mode = params[:view_mode]
-		@work = params[:work]
-		# slurp up the required text
-		@jtext = Text.all.where(work: params[:work]).to_json(:include => :element)
-		# add the default position of 0 for an operator
-	end
+    #set the operator variable
+    @operator = params[:operator]
+    @view_mode = params[:view_mode]
+    @work = params[:work]
+    # slurp up the required text
+    @jtext = Text.all.where(work: params[:work]).to_json(:include => :element)
+    # add the default position of 0 for an operator
+  end
 
 	def blackout
 		#global variable is used for blacking out the display
@@ -46,47 +46,47 @@ class OperatorController < ApplicationController
 		render :json => $current
 	end
 
-	def select
-		#TODO: refactor, break out into several methods
-		@operators = Operator.all
-		if request.get?
-			now = DateTime.now
-			@operators.each do |operator|
+  def select
+    #TODO: refactor, break out into several methods
+    @operators = Operator.all
+    if request.get?
+      now = DateTime.now
+      @operators.each do |operator|
         # if the operator record is more than a day old remove it
-				if operator.updated_at < now-1
-					operator.destroy!
-				end
-			end
-			#pull back new list of operators
-			@operators = Operator.all
-		end
+        if operator.updated_at < now-1
+          operator.destroy!
+        end
+      end
+      #pull back new list of operators
+      @operators = Operator.all
+    end
 
-		if request.post?
-			if !params[:selected_operator].nil?
-				# load exisiting operator session
-				operator = Operator.find_by(id: params[:selected_operator])
-				operator_name = operator.name
-				view_mode = operator.view_attributes
-				work = operator.work_id
-				Rails.application.config.operator_positions.merge!({params[:operator] => "0"})
-			else
-				# Create Operator record
-				operator_name = params[:operator]
-				view_mode = params[:view]
-				work = params[:work]
-				#TODO: check to see if we will be creating a duplicate
+    if request.post?
+      if !params[:selected_operator].nil?
+        # load exisiting operator session
+        operator = Operator.find_by(id: params[:selected_operator])
+        operator_name = operator.name
+        view_mode = operator.view_attributes
+        work = operator.work_id
+        Rails.application.config.operator_positions.merge!({params[:operator] => "0"})
+      else
+        # Create Operator record
+        operator_name = params[:operator]
+        view_mode = params[:view]
+        work = params[:work]
+        #TODO: check to see if we will be creating a duplicate
 
-				operator = Operator.create!(name: operator_name[:name],
-					view_attributes: params[:view], work_id: params[:work], position: 0)
-			end
+        operator = Operator.create!(name: operator_name[:name],
+          view_attributes: params[:view], work_id: params[:work], position: 0)
+      end
 
-			# set position to the value stored in the operator record
-			Rails.application.config.operator_positions.merge!(
-				{operator.id => operator.position})
-			# redirect to index
-			redirect_to :controller => 'operator', :action => 'index',
-				:work => work, :view_mode => view_mode,
-				:operator => operator.id
-		end
-	end
+      # set position to the value stored in the operator record
+      Rails.application.config.operator_positions.merge!(
+        {operator.id => operator.position})
+      # redirect to index
+      redirect_to :controller => 'operator', :action => 'index',
+        :work => work, :view_mode => view_mode,
+        :operator => operator.id
+    end
+  end
 end
--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -3,198 +3,181 @@ class TextsController < ApplicationController
     @work = Work.find(params[:work_id])
   end
 
-        def show
-        end
+  def show
+  end
 
 
-        #********************************************************************
-        # inserts a line into the database. Used for splitting up a monologue
-        # we get the work id, then find the
-        #******************************************************************
-       def splitLine
-          lne = Text.find(params[:id]) #gives us the line
-          wid = lne.work_id #gives us the work id to use for selecting the right whitespace element
-          seqid = lne.sequence
+  #********************************************************************
+  # inserts a line into the database. Used for splitting up a monologue
+  # we get the work id, then find the
+  #******************************************************************
+ def splitLine
+    lne = Text.find(params[:id]) #gives us the line
+    wid = lne.work_id #gives us the work id to use for selecting the right whitespace element
+    seqid = lne.sequence
 
-          #get all lines beyond seqid (sequence starts at seqid +1 and ID will be +2)
-          qry = "work_id = " + wid.to_s + " AND sequence > " + seqid.to_s
-          e_beyond = Text.where(qry)
-          e_beyond.each do |a|
+    #get all lines beyond seqid (sequence starts at seqid +1 and ID will be +2)
+    qry = "work_id = " + wid.to_s + " AND sequence > " + seqid.to_s
+    e_beyond = Text.where(qry)
+    e_beyond.each do |a|
 
-          #increment each line beyond the current one
-          a.increment!(:sequence, by = 1)
-          end
-
-
-          #create new line for character with a copy of the current text
-          txt = Text.create(sequence: (seqid + 1), element_id: lne.element_id, work_id: lne.work_id, content_text: lne.content_text, visibility: lne.visibility)
-          if txt.save
-            flash[:notice] = "SPLIT successfully made"
-            redirect_to:back
-          else
-            flash[:notice] = "SPLIT NOT successfully made!!"           #need to fix this
-            redirect_to:back
-
-          end
-        end
+    #increment each line beyond the current one
+    a.increment!(:sequence, by = 1)
+    end
 
 
+    #create new line for character with a copy of the current text
+    txt = Text.create(sequence: (seqid + 1), element_id: lne.element_id, work_id: lne.work_id, content_text: lne.content_text, visibility: lne.visibility)
+    if txt.save
+      flash[:notice] = "SPLIT successfully made"
+      redirect_to:back
+    else
+      flash[:notice] = "SPLIT NOT successfully made!!"           #need to fix this
+      redirect_to:back
 
-      #********************************************************************
-       #inserts a blank line into the database after seqid of the line selected
-       # we get the work id, then find the
-       #******************************************************************
-       def addBlank
-          lne = Text.find(params[:id]) #gives us the line
-          wid = lne.work_id #gives us the work id to use for selecting the right whitespace element
-          seqid = lne.sequence
+    end
+  end
 
-          #get all lines beyond seqid (sequence starts at seqid +1 and ID will be +2)
-          qry = "work_id = " + wid.to_s + " AND sequence > " + seqid.to_s
-          e_beyond = Text.where(qry)
-          e_beyond.each do |a|
+  #********************************************************************
+  #inserts a blank line into the database after seqid of the line selected
+  # we get the work id, then find the
+  #******************************************************************
+  def addBlank
+    lne = Text.find(params[:id]) #gives us the line
+    wid = lne.work_id #gives us the work id to use for selecting the right whitespace element
+    seqid = lne.sequence
 
-          #increment each line beyond the current one
-          a.increment!(:sequence, by = 1)
-          end
+    #get all lines beyond seqid (sequence starts at seqid +1 and ID will be +2)
+    qry = "work_id = " + wid.to_s + " AND sequence > " + seqid.to_s
+    e_beyond = Text.where(qry)
+    e_beyond.each do |a|
 
-
-          #insert our new blankline element with (seqid + 1)
-          txt = Text.create(sequence: (seqid + 1), element_id: Element.find_by(element_type: 'BLANKLINE').id, work_id: wid, content_text: "<br /> <br />", visibility: true)
-          if txt.save
-            flash[:notice] = "BLANK SPACE successfully added"
-            redirect_to:back
-          else
-            flash[:notice] = "BLANK SPACE NOT added"        # need to fix this
-            redirect_to:back
-
-          end
-        end
+    #increment each line beyond the current one
+    a.increment!(:sequence, by = 1)
+    end
 
 
+    #insert our new blankline element with (seqid + 1)
+    txt = Text.create(sequence: (seqid + 1), element_id: Element.find_by(element_type: 'BLANKLINE').id, work_id: wid, content_text: "<br /> <br />", visibility: true)
+    if txt.save
+      flash[:notice] = "BLANK SPACE successfully added"
+      redirect_to:back
+    else
+      flash[:notice] = "BLANK SPACE NOT added"        # need to fix this
+      redirect_to:back
+
+    end
+  end
+
+  #********************************************************************
+  # removes a line from the database and lines after seqid of the line selected
+  # each line after will be decremented to fil in the gap
+  #******************************************************************
+  def removeLine
+    lne = Text.find_by_id(params[:id]) #gives us the line
+    wid = lne.work_id #gives us the work id to use for selecting the right whitespace element
+    seqid = lne.sequence
 
 
-      #********************************************************************
-       # removes a line from the database and lines after seqid of the line selected
-       # each line after will be decremented to fil in the gap
-       #******************************************************************
-       def removeLine
+    #remove the current line from the database
+    lne.destroy
 
-          lne = Text.find_by_id(params[:id]) #gives us the line
-          wid = lne.work_id #gives us the work id to use for selecting the right whitespace element
-          seqid = lne.sequence
+    #get all lines beyond seqid (sequence starts at seqid +1 and ID will be +2)
+    qry = "work_id = " + wid.to_s + " AND sequence > " + seqid.to_s
+    e_beyond = Text.where(qry)
 
+    e_beyond.each do |a|
 
-          #remove the current line from the database
-          lne.destroy
+    #increment each line beyond the current one
+    a.decrement!(:sequence, by = 1)
+    end
 
-          #get all lines beyond seqid (sequence starts at seqid +1 and ID will be +2)
-          qry = "work_id = " + wid.to_s + " AND sequence > " + seqid.to_s
-          e_beyond = Text.where(qry)
+    redirect_to :controller => 'texts', :action => 'index', :work_id => wid
+  end
 
-          e_beyond.each do |a|
+  #********************************************************************
+  #updates the visibility of a line when the visibility is toggled in editorview
+  #******************************************************************
+  def toggleVis
+    @text2 = Text.find(params[:id])
 
-          #increment each line beyond the current one
-          a.decrement!(:sequence, by = 1)
-          end
+    #sets the visibility from false to true
+    if @text2.visibility == false
+      if @text2.update_attributes(:visibility => true)
+          render :json => 'true'
+      else
+          render :json => 'Failed to update the record to true'
+      end
 
-          redirect_to :controller => 'texts', :action => 'index', :work_id => wid
-        end
+      #sets the visibility from true to false
+    else
+      if @text2.update_attributes(:visibility => false)
+        render :json => 'true'
+      else
+        render :json => 'Failed to update record to false'
+      end
+    end
+  end
 
+  #********************************************************************
+  #updates the fields in the Text table when the editor changes the lines in a script
+  #******************************************************************
+  def update
 
+     @text2 = Text.find(params[:id])
 
-
-       #********************************************************************
-       #updates the visibility of a line when the visibility is toggled in editorview
-       #******************************************************************
-       def toggleVis
-
-
-          @text2 = Text.find(params[:id])
-
-        #sets the visibility from false to true
-          if @text2.visibility == false
-
-              if @text2.update_attributes(:visibility => true)
-                  render :json => 'true'
-              else
-                  render :json => 'Failed to update the record to true'
-              end
-
-        #sets the visibility from true to false
-          else
-              if @text2.update_attributes(:visibility => false)
-                render :json => 'true'
-              else
-                render :json => 'Failed to update record to false'
-              end
-
-          end
-       end
-
-
-
-
-
-       #********************************************************************
-       #updates the fields in the Text table when the editor changes the lines in a script
-       #******************************************************************
-       def update
-
-          @text2 = Text.find(params[:id])
-
-          if @text2.update_attributes(message_params)
-	          # Handle a successful update.
-	          flash[:notice] = "Text successfully updated"
-	          redirect_to:back
-          else
-          	flash[:notice] = "NOTICE: ERROR DURING UPDATE"
-	          redirect_to:back
-          end
-       end
-
-
-       #********************************************************************
-       # adds a visible line with text for a specified character in the form 
-       # beyond the line selected to insert at
-       #******************************************************************
-       def addLine
-        char_text = params['new_line']
-
-          lne = Text.find_by_id(params[:id]) #gives us the line we will insert after
-          wid = lne.work_id #gives us the work id to use for selecting the right whitespace element
-          seqid = lne.sequence
-
-          #get all lines beyond seqid (sequence starts at seqid +1 and ID will be +2)
-          qry = "work_id = " + wid.to_s + " AND sequence > " + seqid.to_s
-          e_beyond = Text.where(qry)
-          e_beyond.each do |a|
-
-          #increment each line beyond the current one
-          a.increment!(:sequence, by = 1)
-          end
-
-          #insert our new line for character with text and (seqid + 1)
-          txt = Text.create(sequence: (seqid + 1), element_id: Element.find_by_id(params['character_name_dropdown']).id, work_id: wid, content_text: char_text['content_text'], visibility: true)
-          if txt.save
-            flash[:notice] = "New character line successfully added"
-            redirect_to:back
-          else
-            flash[:notice] = "New Line NOT added"        # need to fix this
-             redirect_to:back
-
-          end
-       end
-    private
-
-       #********************************************************************
-       #used for bringing in params sent to the update function
-       #******************************************************************
-	   def message_params
-	      params.require(:text).permit(:content_text, :visibility)
-	   end
-
-      def vis_params
-        params.require(:id).permit(:id)
+     if @text2.update_attributes(message_params)
+       # Handle a successful update.
+       flash[:notice] = "Text successfully updated"
+       redirect_to:back
+     else
+       flash[:notice] = "NOTICE: ERROR DURING UPDATE"
+       redirect_to:back
      end
+  end
+
+
+  #********************************************************************
+  # adds a visible line with text for a specified character in the form
+  # beyond the line selected to insert at
+  #******************************************************************
+  def addLine
+   char_text = params['new_line']
+
+     lne = Text.find_by_id(params[:id]) #gives us the line we will insert after
+     wid = lne.work_id #gives us the work id to use for selecting the right whitespace element
+     seqid = lne.sequence
+
+     #get all lines beyond seqid (sequence starts at seqid +1 and ID will be +2)
+     qry = "work_id = " + wid.to_s + " AND sequence > " + seqid.to_s
+     e_beyond = Text.where(qry)
+     e_beyond.each do |a|
+
+     #increment each line beyond the current one
+     a.increment!(:sequence, by = 1)
+     end
+
+     #insert our new line for character with text and (seqid + 1)
+     txt = Text.create(sequence: (seqid + 1), element_id: Element.find_by_id(params['character_name_dropdown']).id, work_id: wid, content_text: char_text['content_text'], visibility: true)
+     if txt.save
+       flash[:notice] = "New character line successfully added"
+       redirect_to:back
+     else
+       flash[:notice] = "New Line NOT added"        # need to fix this
+        redirect_to:back
+     end
+  end
+
+  private
+
+  #********************************************************************
+  #used for bringing in params sent to the update function
+  #******************************************************************
+  def message_params
+    params.require(:text).permit(:content_text, :visibility)
+  end
+
+  def vis_params
+    params.require(:id).permit(:id)
+  end
 end
