80a2e9483c37c35ab3b184297cbcbca9a00fc402
Randolph Jones <randolphjones1029@gmail.com>
2015-02-24 01:39:37 -0800

    created basic line moving function

--- app/assets/javascripts/display.js
+++ app/assets/javascripts/display.js
@@ -1,20 +1,66 @@
 //javascript for the display view goes here
-var refresh = 500;
-var dispFadeSpd = 400;
-var displayScrollSpd = 1200;
+
+//need a remapping function to predict character limites from scaling
+function remap(v, min, max, nMin, nMax){
+	return nMin + (nMax - nMin) * (v - min) / (max - min);
+}
+
+var refresh = 20;
+var dispFadeSpd = 40;
+var displayScrollSpd = 500;
 var $linesDisp;
+
+//set a general scale number to go by
+//900 represents 900% font size and will use this to parse and divide text elements globally
+var scale = 900;
+//30 characters fill the appropriate width of a 1080P screen at 900%
+//50 characters fill the appropriate width of a 1080P screen at 600%
+//will want to find the next space inside of 30 characters and split lines there
+//var charLim = Math.round(remap(scale, 600, 900, 50, 30));
+var $b, inc, cur;
+//make the page move at any given increment
+//increment with a negative to move down
+function move($el, incr){
+	cur+=incr;
+	$el.animate({scrollTop:cur}, displayScrollSpd);
+}
 $(document).ready(function(){
+
 	if($('#body-index-display').length>0){
+		$b = $('#body-index-display');
+		cur = $('#body-index-display').scrollTop();
+
 		var current=0;
 
 		//make sure the lines are sorted by sequence instead of index when read in
 		lines = _.sortBy(lines,function(q){
 			return q.sequence;
 		});
+/*
+		//split lines by char limit here?
+		var i = lines.length-1;
+		while(i>=0){
+			console.log(lines[i]['content_text']);
+			var c = lines[i]['content_text'];
+			var arr = [];
+			while(c.length>0){
+				arr.push(c.substr(0,charLim));
+
+			}
+			lines[i]['content_arr']=arr;
+
+			i--;
+		}
+		*/
+
 		function buildLinesDisp($container){
+
+
+
 			var il = 0;
 			var ll = lines.length; 
 			var lc, cc;
+			//figure out if we're going to include character name based on who said last line
 			while(il<ll){
 				if(lines[il]['visibility']){
 					var cl = lines[il];
@@ -30,7 +76,7 @@ $(document).ready(function(){
 				il++;
 			}
 		}
-
+		//if we're in single line view...
 		if($('#multi-flag-display').length<=0){
 			$lineCont = $('#line-holder-display');
 			// template views
@@ -53,8 +99,6 @@ $(document).ready(function(){
 			//build the line dom
 			buildLinesDisp($lineCont);
 			$linesDisp = $('.line-display');
-			//when the first line fade in it sets off the preiodic ajax scrape
-			//for now we're just grabbing a random number but soon it will be the number supplied by the operator
 			$linesDisp.first().fadeIn(dispFadeSpd, function(){
 				$(this).addClass('shown-display');
 				//set first interval
@@ -102,10 +146,17 @@ $(document).ready(function(){
 
 		//end of original block
 		//}
+
+		//if we're using multi-line view
 		}else{
+
 			$('#shade-multi').css('display', 'none');
 			//this is specific JS for the multi-line view
-			console.log('welcome to multi-line mode');
+			//console.log('welcome to multi-line mode');
+
+			//redivide lines
+
+
 			// //template views
 			var tLine = _.template($('#line-template-display-multi').html());
 
@@ -129,6 +180,9 @@ $(document).ready(function(){
 
 			//build the line dom
 			buildLinesDisp($lineCont);
+
+			//set increment for line up/down
+			inc = parseFloat($('.line-display-multi').css('line-height'));
 			$linesDisp = $('.line-display-multi');
 
 
