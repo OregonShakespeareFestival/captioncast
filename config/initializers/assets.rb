ea7751b976db567a96de5e5ecf4dbe11a8733463
Jason DeShazer <deshazer.jad@gmail.com>
2015-03-05 10:34:07 -0800

    updated assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.compile = true
 Rails.application.config.assets.digest  = true
 Rails.application.config.assets.precompile =  %w( '*.css' )
 Rails.application.config.assets.precompile += %w( '*.css.erb' )
+Rails.application.config.assets.precompile += %w( application.css )
 Rails.application.config.assets.precompile += %w( 'rails_admin/rails_admin.css' )
 Rails.application.config.assets.precompile += %w( 'rails_admin/rails_admin.js' )
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
