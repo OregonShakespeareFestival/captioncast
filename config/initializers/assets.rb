26c3e0f4973be97cd562505726eb075f9787239d
toeknee919 <toeknee919@gmail.com>
2015-04-13 23:27:46 -0700

    added editor display session

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,17 +1,20 @@
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
+# Version of your assets, change this if you want to expire all your assets.		
 Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 
 Rails.application.config.assets.compile = true
-Rails.application.config.assets.digest  = true
-Rails.application.config.assets.precompile =  %w( '*.css' )
-Rails.application.config.assets.precompile += %w( '*.css.erb' )
-Rails.application.config.assets.precompile += %w( application.css )
-Rails.application.config.assets.precompile += %w( 'rails_admin/rails_admin.css' )
-Rails.application.config.assets.precompile += %w( 'rails_admin/rails_admin.js' )
+
+#BELOW SHOULD BE UNCOMMENTED FOR PRODUCTION MODE
+#Rails.application.config.assets.digest  = true
+#Rails.application.config.assets.precompile =  %w( '*.css' )
+#Rails.application.config.assets.precompile += %w( '*.css.erb' )
+#Rails.application.config.assets.precompile += %w( application.css )
+#Rails.application.config.assets.precompile += %w( 'rails_admin/rails_admin.css' )
+#Rails.application.config.assets.precompile += %w( 'rails_admin/rails_admin.js' )
+
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( application.js )
 Rails.application.config.assets.precompile += %w( cast.js )
