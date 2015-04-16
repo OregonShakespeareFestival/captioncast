645cd71c6599668814af3cd6ecfef9a4f3a2e666
Jason DeShazer <deshazer.jad@gmail.com>
2015-04-16 16:07:39 -0700

    changed 0 to 1

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -8,7 +8,7 @@ class PreviewController < ApplicationController
   	# get the current line sequence number
     # if the current line sequence number doesn't exist
   	if(Rails.application.config.operator_positions[params[:operator]] == nil)
-      line_sequence_number = 0 # throw back 0
+      line_sequence_number = 1 # throw back 1
     else
       line_sequence_number = Rails.application.config.operator_positions[params[:operator]] # get the current line sequence number
     end
@@ -21,8 +21,8 @@ class PreviewController < ApplicationController
     character_current = Element.find_by(:id => line_current_record.pluck(:element_id)).name
 
     # get the previous line record and character name
-    # if the line sequence isn't at 0
-    if(line_sequence_number != "0")
+    # if the line sequence isn't at 1
+    if(line_sequence_number != "1")
   	  line_previous_record = work.texts.where(:sequence => line_sequence_number.to_i - 1)
   	  character_previous = Element.find_by(:id => line_previous_record.pluck(:element_id)).name
     end
@@ -30,7 +30,7 @@ class PreviewController < ApplicationController
     # set the content to be display initially as a string without name attached
     content = line_current_record.pluck(:content_text)[0]
     # check if the name needs to be attached. if so, attach it
-    if(line_sequence_number != "0")
+    if(line_sequence_number != "1")
     	if(character_current != character_previous)
     		content =  character_current.to_s + ": " + line_current_record.pluck(:content_text)[0]
     	end
