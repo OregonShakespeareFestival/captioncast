addf3dfe1260d030d862ba45f8b69fd9f0deb90b
Jason DeShazer <deshazer.jad@gmail.com>
2015-04-16 15:36:51 -0700

    safegaurd against pushing line seq number 0

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -6,19 +6,35 @@ class PreviewController < ApplicationController
 
   def getLineSequence
   	# get the current line sequence number
-  	line_sequence_number = Rails.application.config.operator_positions[params[:operator]]
-  	# get the current and previous line and character
+    # if the current line sequence number doesn't exist
+  	if(Rails.application.config.operator_positions[params[:operator]] == nil)
+      line_sequence_number = 0 # throw back 0
+    else
+      line_sequence_number = Rails.application.config.operator_positions[params[:operator]] # get the current line sequence number
+    end
+
+  	# get the current work
   	work = Work.find_by(:id => params[:work])
+
+    # use the current work to get the current line record and character name
   	line_current_record = work.texts.where(:sequence => line_sequence_number)
-  	line_previous_record = work.texts.where(:sequence => line_sequence_number.to_i - 1)
-  	character_current = Element.find_by(:id => line_current_record.pluck(:element_id)).name
-  	character_previous = Element.find_by(:id => line_previous_record.pluck(:element_id)).name
-  	# check if previous line is spoken by different character
-  	if(character_current != nil && character_previous != nil && character_current != character_previous)
-  		content =  character_current.to_s + ": " + line_current_record.pluck(:content_text)[0]
-  	else
-  		content = line_current_record.pluck(:content_text)[0]
-  	end
+    character_current = Element.find_by(:id => line_current_record.pluck(:element_id)).name
+
+    # get the previous line record and character name
+    # if the line sequence isn't at 0
+    if(line_sequence_number != "0")
+  	  line_previous_record = work.texts.where(:sequence => line_sequence_number.to_i - 1)
+  	  character_previous = Element.find_by(:id => line_previous_record.pluck(:element_id)).name
+    end
+
+    # set the content to be display initially as a string without name attached
+    content = line_current_record.pluck(:content_text)[0]
+    # check if the name needs to be attached. if so, attach it
+    if(line_sequence_number != "0")
+    	if(character_current != character_previous)
+    		content =  character_current.to_s + ": " + line_current_record.pluck(:content_text)[0]
+    	end
+    end
 
   	# render current line
   	render :json => { :content => content}
