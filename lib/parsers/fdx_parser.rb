d9aaa5f98c583f4b34e43522274176e25af353b4
Tony <toeknee919@gmail.com>
2016-01-28 10:22:42 -0800

    fine tuning for fdx parser

--- lib/parsers/fdx_parser.rb
+++ lib/parsers/fdx_parser.rb
@@ -3,11 +3,12 @@ require 'nokogiri'
 class FDXParser < BaseParser
 
   def parse(file)
+    logger = Logger.new('logfile.log')
     ActiveRecord::Base.transaction do
       doc =  Nokogiri::XML(file)
       # add non dialogue elements
       doc.xpath("/FinalDraft/Content/*[not(@Type='Dialogue')]").each do |nondialogue|
-        type = nondialogue.attributes["Type"].value
+        type = nondialogue.attributes["Type"].value.strip
         Element.find_or_create_by!(element_name: "", element_type: type.to_s.upcase, color: @default_text_color, work: @work_id)
       end
       # add "BLANKLINE" and "OPERATOR_NOTE" elements
@@ -26,11 +27,11 @@ class FDXParser < BaseParser
             # add previous character's text to database
             text_components = segment_text(current_text, current_character)
             text_components.each do |text_component|
-              Text.create(sequence: text_sequence, element: Element.find_by(element_name: current_character, element_type: "CHARACTER"), work: @work_id, content_text: text_component, visibility: true)
+              Text.create(sequence: text_sequence, element: Element.find_by(element_name: current_character, element_type: "CHARACTER", work_id: @work_id), work: @work_id, content_text: text_component, visibility: true)
               text_sequence += 1
             end
           end
-          current_character = line.text.delete("\n").squeeze(" ").strip
+          current_character = line.text.delete("\n").squeeze(" ").strip.upcase
           Element.find_or_create_by!(element_name: current_character, element_type: "CHARACTER", color: @default_text_color, work: @work_id)
           current_text = ""
         elsif line_type == "DIALOGUE"
@@ -43,13 +44,17 @@ class FDXParser < BaseParser
             #currently we dont want to see either of these in the database
         else
           direction = line.text.delete("\n").squeeze(" ").strip
-          Text.create(sequence: text_sequence, element: Element.find_by(element_type: line_type), work: @work_id, content_text: direction, visibility: false)
+          begin
+            Text.create!(sequence: text_sequence, element: Element.find_by(element_name: "", element_type: line_type,  work_id: @work_id), work: @work_id, content_text: direction, visibility: false)
+          rescue => ex
+            logger.error {ex.message}
+          end
           text_sequence += 1
         end
       end
       text_components = segment_text(current_text, current_character)
       text_components.each do |text_component|
-        Text.create(sequence: text_sequence, element: Element.find_by(element_name: current_character, element_type: "CHARACTER"), work: @work_id, content_text: text_component, visibility: true)
+        Text.create(sequence: text_sequence, element: Element.find_by(element_name: current_character, element_type: "CHARACTER",  work_id: @work_id), work: @work_id, content_text: text_component, visibility: true)
         text_sequence += 1
       end
     end
