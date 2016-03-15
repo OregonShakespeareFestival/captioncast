5ef6b95bd207b4be86cb4348a61132e10567c076
Tony <toeknee919@gmail.com>
2016-03-15 08:39:19 -0700

    updated text upload script to accept blankline

--- lib/parsers/txt_parser.rb
+++ lib/parsers/txt_parser.rb
@@ -1,11 +1,16 @@
 class TXTParser < BaseParser
 
   def parse(file)
+    # add "BLANKLINE" and "OPERATOR_NOTE" elements
+    Element.find_or_create_by!(element_name: "", element_type: "BLANKLINE", color: @default_text_color, work: @work_id)
+    Element.find_or_create_by!(element_name: "", element_type: "OPERATOR_NOTE", color: @default_text_color, work: @work_id)
+
     ActiveRecord::Base.transaction do
       text_sequence = 1
       current_line_number = 1
       current_text = ""
       current_character = ""
+      added_blank = false
       # iterate over each line in file
       file.each_line do |line|
         current_line_number += 1
@@ -13,8 +18,8 @@ class TXTParser < BaseParser
         line_components = line.split(":", 2)
         # line contains character and text
         if line_components.length == 2
-          # if not the first character
-          if not current_character == ""
+          # if we know who is speaking
+          if current_character != "" && added_blank == false
             # add previous character's text to database
             text_components = segment_text(current_text, current_character)
             text_components.each do |text_component|
@@ -25,9 +30,36 @@ class TXTParser < BaseParser
           current_character = line_components[0].strip
           current_text = line_components[1].strip
           Element.find_or_create_by(element_name: current_character, element_type: "CHARACTER", color: @default_text_color, work: @work_id)
-        # line contains text
+          # Reset indicator
+          added_blank = false
+
+        # line contains only text
         elsif line_components.length == 1
-          current_text << " " << line_components[0].strip
+          
+          # If we recently added a blankline and need to pick up where we left off in a characters script
+          if added_blank == true && line_components[0].strip != "##"
+            current_text = line_components[0].strip
+            # reset blank indicator
+            added_blank = false
+
+
+          # If this is a blankline
+          elsif line_components[0].strip == "##"
+            # add previous character's already built text to database
+            text_components = segment_text(current_text, current_character)
+            text_components.each do |text_component|
+              Text.create(sequence: text_sequence, element: Element.find_by(element_name: current_character, element_type: "CHARACTER"), work: @work_id, content_text: text_component, visibility: true)
+              text_sequence += 1
+            end
+            # Set the indicator
+            added_blank = true 
+
+            # then add our blankline
+            Text.create(sequence: text_sequence, element: Element.find_by(element_name: "", element_type: "BLANKLINE"), work: @work_id, content_text: "<br/>", visibility: true)
+            text_sequence += 1
+          else
+            current_text << " " << line_components[0].strip
+          end
         else
           raise current_line_number.to_s << ": " << line
         end
