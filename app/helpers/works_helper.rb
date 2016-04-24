b12d27591523238e8dba5571dc0b19da8bcb71a1
Jason DeShazer <deshazer.jad@gmail.com>
2016-04-24 10:11:12 -0700

    added bootstrap style to works#index view

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -2,6 +2,7 @@ module WorksHelper
   def work_details(work)
     string = "#{work.work_name}: #{work.language}"
     string += " (#{pluralize(work.texts.length, 'text')})" if work.texts.present?
+    string += " (uploading...)" if work.uploading
     string
   end
 end
