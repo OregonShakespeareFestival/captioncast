93544a2687732bf22a07829cf6aafd50814309e0
Jason DeShazer <deshazer.jad@gmail.com>
2016-04-24 11:26:44 -0700

    added uploading indicator to works#index view

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -2,7 +2,6 @@ module WorksHelper
   def work_details(work)
     string = "#{work.work_name}: #{work.language}"
     string += " (#{pluralize(work.texts.length, 'text')})" if work.texts.present?
-    string += " (uploading...)" if work.uploading
     string
   end
 end
