d001cf9c00547cb016ba0ec8c1539439379bd580
Jason DeShazer <deshazer.jad@gmail.com>
2016-05-15 15:34:10 -0700

    authenticate the user against OSF crowd server

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,10 +1,28 @@
 class AuthController < ApplicationController
+  require 'rest-client'
+  require 'xmlsimple'
 
   def new
   end
 
   def create
-    session[:user] = "test"
+    # generate rest request for authentication with crowd
+    rest_auth = "Basic " + Base64.encode64("broken-props:admin")
+    rest_url = "https://crowd.osfashland.org/crowd/rest/usermanagement/latest/authentication?username=#{params[:username]}"
+    rest_body = "<?xml version='1.0' encoding='UTF-8'?><password><value>#{params[:password]}</value></password>"
+    # send request
+    begin
+      response = RestClient.post(rest_url, rest_body, :Content_Type => :xml, :Accept => :xml, :Authorization => rest_auth)
+    rescue => e
+      # failed login
+      redirect_to "/login"
+      return
+    end
+    # get the user hash from the response
+    response_hash = XmlSimple.xml_in(response)
+    # set the current user's display name
+    session[:user] = response_hash['display-name'][0]
+    # redirect back to the homepage
     redirect_to "/"
   end
 
